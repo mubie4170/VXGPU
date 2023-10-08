372957157a5605f5c59a7e8d650267e2545c92bc
mubie4170 <146758855+mubie4170@users.noreply.github.com>
2023-10-08 23:23:22 +0100

    Update vast.sh

--- vast.sh
+++ vast.sh
@@ -45,7 +45,7 @@ sleep 5
 echo "---------------------------"
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xf31d61695bbd8179d8d5bad51b8cc3ec0cc93f72/g' config.conf
 sleep 5
 echo "---------------------------"
 
